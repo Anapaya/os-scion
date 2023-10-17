1d1980ef19bc8290fd85f59932c51a56b6ac0cae
Dominik Roos <roos@anapaya.net>
2023-10-17 17:29:40 +0200

    PATCH: patches/com_github_scionproto_scion/quic_scion_version.patch

--- private/path/combinator/graph.go
+++ private/path/combinator/graph.go
@@ -443,7 +443,10 @@ func (solution *pathSolution) Path(hashState hashState) Path {
 	staticInfo := collectMetadata(interfaces, asEntries)
 
 	path := Path{
-		SCIONPath: segments.ScionPath(),
+		SCIONPath: snetpath.SCION{
+			Raw: segments.ScionPath(),
+			MTU: mtu,
+		},
 		Metadata: snet.PathMetadata{
 			Interfaces:   interfaces,
 			MTU:          mtu,
@@ -657,7 +660,7 @@ func (s segmentList) ComputeExpTime() time.Time {
 	return minTimestamp
 }
 
-func (s segmentList) ScionPath() snetpath.SCION {
+func (s segmentList) ScionPath() []byte {
 	var meta scion.MetaHdr
 	var infos []path.InfoField
 	var hops []path.HopField
@@ -680,5 +683,5 @@ func (s segmentList) ScionPath() snetpath.SCION {
 	if err := sp.SerializeTo(raw); err != nil {
 		panic(err)
 	}
-	return snetpath.SCION{Raw: raw}
+	return raw
 }
