5017744932b81e480c435b0a28827c4db92aa2ec
Martin Sustrik <sustrik@anapaya.net>
2024-01-05 14:47:12 +0100

    PATCH: patches/com_github_scionproto_scion/metrics_reset_fix.patch
    Reset deleted all the timeseries irrespective of the labels set.
    With this patch only the timeseries matching the labels are reset.
    
    Reset for histograms is removed as prometheus client does not
    implement reset on ObserverVec.

--- pkg/metrics/prometheus.go
+++ pkg/metrics/prometheus.go
@@ -120,7 +120,7 @@ func (g *gauge) Add(delta float64) {
 
 // Reset deletes all metrics in this vector.
 func (g *gauge) Reset() {
-	g.gv.Reset()
+	g.gv.MustCurryWith(makeLabels(g.lvs...)).Reset()
 }
 
 // newGauge wraps the GaugeVec and returns a usable Gauge object.
@@ -166,7 +166,7 @@ func (c *counter) Add(delta float64) {
 
 // Reset deletes all metrics in this vector.
 func (c *counter) Reset() {
-	c.cv.Reset()
+	c.cv.MustCurryWith(makeLabels(c.lvs...)).Reset()
 }
 
 // histogram implements Histogram via a Prometheus HistogramVec. The difference
@@ -203,11 +203,6 @@ func (h *histogram) With(labelValues ...string) Histogram {
 // Observe implements Histogram.
 func (h *histogram) Observe(value float64) {
 	h.hv.With(makeLabels(h.lvs...)).Observe(value)
-}
-
-// Reset deletes all metrics in this vector.
-func (h *histogram) Reset() {
-	h.hv.Reset()
 }
 
 func makeLabels(labelValues ...string) prometheus.Labels {
